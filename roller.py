6b70401e2b1392a5672404726ea12875b1e554bf
CSelwood <cselwood1806@hotmail.co.uk>
2021-02-07 14:46:53 +0000

    Reformatted and Wiped Roller
    Reformatted to work as OOP program. Clearly labeled into sections.

--- roller.py
+++ roller.py
@@ -1,84 +1,49 @@
 # Imports
-import tkinter as tk
+from tkinter import *
 import fontTools.ttLib
 import random
 
-class titleFrame():
-    def __init__(self):
-        print("Hi")
-
-class mainWindow:
-    window = tk.Tk()
-    program_name="D&D Diceroller!"
-
-    main_label_ending = " Die!"
-    number_of_dice = "One"
-    dice_sides = 20
-    width = 1000
-    height = 600
-    default_roll = 20
-
-    background_color = "#026670"
-    text_color = "#edeae5"
-    highlighted_color = "#9fedd7"
-
-    number_to_words = { 0: "Zero",
-                        1: "One",
-                        2: "Two",
-                        3: "Three",
-                        4: "Four",
-                        5: "Five",
-                        6: "Six",
-                        8: "Eight",
-                        10: "Ten",
-                        12: "Twelve",
-                        20: "Twenty",
-                        100: "One Hundred"}
-
-    window.resizable(width=False, height=False)
-    window.minsize(width=width, height=height)
-    window.configure(background=background_color)
-    window.iconbitmap('favicon.ico')
-    window.title(program_name)
-
-    main_label = tk.Label(window)
-    main_label.configure(background=background_color, font=("Garamond Bold", 30), height=2, fg=text_color)
-
-    dice_label = tk.Label(window, text=default_roll)
-    dice_label.configure()
-
-    # Create Layout
-    main_label.grid(column=0, row=0)
-    window.columnconfigure(0, weight=1)
-
-    def updateMainLabel(self):
-
-        if self.number_of_dice == 1:
-            self.main_label_ending = " Dice!"
-        else:
-            self.main_label_ending = " Die!"
-
-        self.main_label_full = "Rolling " + self.number_of_dice + " " + str(self.dice_sides) + " Sided" + str(self.main_label_ending)
-        self.main_label.configure(text=self.main_label_full)
-
-# Class Declarations
-class Dice:
-    def __init__(self, value, sides):
-        self.value = value
-        self.sides = sides
-
-# Method Declarations
+# Helper Methods
+# ====================================================
 def convertToWord(amount):
     return number_to_words[amount];
 
 def generateDice(amount, type):
     print("hi")
 
-# Beginning Program
-def main():
-    diceWindow = mainWindow()
-    diceWindow.updateMainLabel()
-    diceWindow.window.mainloop()
+# Model Declarations
+# ====================================================
+class Dice:
+    def __init__(self, value, sides):
+        self.value = value
+        self.sides = sides
+
+# Frames and Classes
+# ====================================================
+class MainFrameLeft():
+    def __init__(self):
+        print("Hi")
+
+class MainFrameRight():
+    def __init__(self):
+        print("Hi")
+
+class MainWindow():
+    def __init__(self, master):
+        self.master = master
+        master.title = "D&D Diceroller!"
+
+# Main
+# ====================================================
+class AppWindow:
+    def __init__(self, parent):
+        self.parent = parent
+        self.mainFrames()
+
+    def mainFrames(self):
+        diceWindow = MainWindow()
 
 if __name__ == '__main__':
-    main()
+    window = Tk()
+    app = AppWindow(window)
+    window.mainloop()
